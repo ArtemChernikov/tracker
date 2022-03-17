f2be0b02451b0fa3b2b1e6fe5b547c3e9fd48663
OvercomingJunk <overcomingjunk@yandex.ru>
2022-03-17 14:12:43 +0300

    Конструктор

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Active: " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        Error error2 = new Error(true, 254, "ArrayIndexOutOfBoundException");
+        Error error3 = new Error(false, 54, "IOException");
+        Error error4 = new Error(true, 21, "RuntimeException");
+        error1.printInfo();
+        error2.printInfo();
+        error3.printInfo();
+        error4.printInfo();
+    }
+}
