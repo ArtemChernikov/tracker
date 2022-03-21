882dfea5237347b7aa036a42537aeb874415f721
OvercomingJunk <overcomingjunk@yandex.ru>
2022-03-21 07:14:12 +0300

    Tracker - хранилище

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,20 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    Item[] findByName(String key) {
+        Item[] rsl = Arrays.copyOf(items, size);
+        int length = 0;
+        for (int i = 0; i < rsl.length; i++) {
+            if (rsl[i].getName().equals(key)) {
+                rsl[length++] = rsl[i];
+            }
+        }
+        return Arrays.copyOf(rsl, length);
     }
 }
