b8694cadb472079fa112f7cc8cb1d03cc98dc640
OvercomingJunk <overcomingjunk@yandex.ru>
2022-03-22 13:41:30 +0300

    Метод удаления заявки Tracker.delete

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -57,7 +57,6 @@ public class Tracker {
     public boolean delete(int id) {
         int index = indexOf(id);
         if (index != -1) {
-            items[index] = null;
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
