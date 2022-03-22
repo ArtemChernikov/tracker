db996f6bc367c37d78f1de680470f059fa8d8d8a
OvercomingJunk <overcomingjunk@yandex.ru>
2022-03-22 07:15:49 +0300

    Реализация класса StartUI. Изменение заявки

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -34,7 +34,6 @@ public class StartUI {
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 if (tracker.replace(id, item)) {
-                    tracker.replace(id, item);
                     System.out.println("Заявка изменена успешно.");
                 } else {
                     System.out.println("Ошибка замены заявки.");
