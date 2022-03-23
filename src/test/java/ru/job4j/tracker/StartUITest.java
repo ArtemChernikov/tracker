fdb8c77dfb0477fe8dbd20169d90d6e74f722f13
OvercomingJunk <overcomingjunk@yandex.ru>
2022-03-23 15:19:13 +0300

    Тестирование. Подготовка данных.

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -17,4 +17,31 @@ public class StartUITest {
         Item expected = new Item("Fix PC");
         assertThat(created.getName(), is(expected.getName()));
     }
+
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()),
+                "edited item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        assertThat(edited.getName(), is("edited item"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("PC");
+        tracker.add(item1);
+        String[] answers = {
+                String.valueOf(item1.getId())
+        };
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = null;
+        assertThat(tracker.findById(item1.getId()), is(deleted));
+    }
 }
