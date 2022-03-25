69d618c79c68412980a387ffcf920a66ce1bd823
OvercomingJunk <overcomingjunk@yandex.ru>
2022-03-25 21:19:14 +0300

    Зависимость от System.out

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -67,8 +67,8 @@ public class StartUITest {
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString(), is(
-                "Menu." + System.lineSeparator() +
-                        "0. Exit" + System.lineSeparator()
+                "Menu." + System.lineSeparator()
+                       + "0. Exit" + System.lineSeparator()
         ));
     }
 }
