a1546a1767583371b0966eec48f00590836e9594
OvercomingJunk <overcomingjunk@yandex.ru>
2022-03-28 23:07:52 +0300

    final

---
+++ src/main/java/ru/job4j/ooa/Airport.java
@@ -0,0 +1,14 @@
+package ru.job4j.ooa;
+
+public class Airport {
+    public static void main(String[] args) {
+        final Airbus airbus = new Airbus("A320");
+        System.out.println(airbus);
+        airbus.printModel();
+        airbus.printCountEngine();
+
+        airbus.setName("A380");
+        airbus.printCountEngine();
+        System.out.println(airbus);
+    }
+}
