a6a8b015188f4494cfee860b5e365242824417e3
OvercomingJunk <overcomingjunk@yandex.ru>
2022-04-10 11:35:48 +0300

    Очередь с приоритетом на LinkedList

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -7,13 +7,12 @@ public class PriorityQueue {
 
     public void put(Task task) {
         int index = 0;
-            for (Task element : tasks) {
-                if (element.getPriority() <= task.getPriority()) {
-                    index++;
-                    continue;
-                }
+        for (Task element : tasks) {
+            if (element.getPriority() > task.getPriority()) {
                 break;
             }
+            index++;
+        }
         this.tasks.add(index, task);
     }
 
