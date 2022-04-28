6777bb4d432ba17fb4ea616a4a1bd688e81541a0
OvercomingJunk <overcomingjunk@yandex.ru>
2022-04-28 21:07:12 +0300

    Зона видимости в лямбда-выражениях и исключения

---
+++ src/main/java/ru/job4j/lambda/ScopeInside.java
@@ -0,0 +1,35 @@
+package ru.job4j.lambda;
+
+import java.util.function.Supplier;
+
+/**
+ * Класс описывает подсчет и вывод суммы чисел в массиве в консоль
+ *
+ * @author ARTEM CHERNIKOV
+ * @version 1.0
+ */
+public class ScopeInside {
+    public static void main(String[] args) {
+        int[] number = {1, 2, 3};
+        int total = 0;
+        for (int i = 0; i < number.length; i++) {
+            int num = number[i];
+            int finalTotal = total;
+            total = add(
+                    () -> finalTotal + num
+            );
+        }
+        System.out.println(total);
+    }
+
+    /**
+     * Метод описывает получение значения с помощью
+     * функционального интерфейса {@link Supplier} и его метода {@link Supplier#get()}
+     *
+     * @param calc - {@link Supplier}
+     * @return - возвращает число с помощью метода {@link Supplier#get()}
+     */
+    private static Integer add(Supplier<Integer> calc) {
+        return calc.get();
+    }
+}
