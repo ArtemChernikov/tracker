66e166f90e6573b424511f73d6523d98adf40ef5
OvercomingJunk <overcomingjunk@yandex.ru>
2022-04-28 20:18:51 +0300

    Лямбда блок

---
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,26 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Класс описывает применение лямбда выражения с выводом отладочной информации
+ * для сортировки списка по длине, по убыванию
+ *
+ * @author ARTEM CHERNIKOV
+ * @version 1.0
+ */
+public class LambdaUsage {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("eeeee", "a", "ccc", "dddd", "bb");
+        Comparator<String> comparator = (left, right) -> {
+            System.out.println("compare - " + right.length() + " : " + left.length());
+            return Integer.compare(right.length(), left.length());
+        };
+        strings.sort(comparator);
+        for (String str : strings) {
+            System.out.println(str);
+        }
+    }
+}
