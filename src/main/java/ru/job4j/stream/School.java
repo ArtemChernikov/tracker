31d1af199a468813d4623646afb6e25bcf6f8bcd
OvercomingJunk <overcomingjunk@yandex.ru>
2022-05-04 21:48:11 +0300

    Фильтрация учеников

---
+++ src/main/java/ru/job4j/stream/School.java
@@ -0,0 +1,27 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * Класс описывает применение потоков для работы с коллекциями
+ *
+ * @author ARTEM CHERNIKOV
+ * @version 1.0
+ */
+public class School {
+    /**
+     * Метод описывает разделение студентов на группы при помощи
+     * функционального интерфейса {@link Predicate}
+     *
+     * @param students - список студентов
+     * @param predict  - функциональный интерфейс {@link Predicate}
+     * @return - возвращает отсортированный список студентов
+     */
+    public List<Student> collect(List<Student> students, Predicate<Student> predict) {
+        return students.stream()
+                .filter(predict)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/ru/job4j/stream/Student.java
@@ -0,0 +1,78 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+/**
+ * Класс модели студента со свойствами <b>score</b> и <b>surname</b>
+ *
+ * @author ARTEM CHERNIKOV
+ * @version 1.0
+ */
+public class Student {
+    /**
+     * Поле успеваемости студента
+     */
+    private int score;
+    /**
+     * Поле фамилии студента
+     */
+    private String surname;
+
+    /**
+     * Конструктор - создание нового объекта с определенными значениями
+     *
+     * @param score   - успеваемость
+     * @param surname - фамилмя
+     */
+    public Student(int score, String surname) {
+        this.score = score;
+        this.surname = surname;
+    }
+
+    /**
+     * Метод получения значения поля {@link Student#score}
+     *
+     * @return - успеваемость
+     */
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     * Метод получения значения поля {@link Student#surname}
+     *
+     * @return - фамилия
+     */
+    public String getSurname() {
+        return surname;
+    }
+
+    /**
+     * Переопределенный метод, для корректного сравнения объектов
+     *
+     * @param o - другой студент
+     * @return возвращает true, если объекты равны, false если нет
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Student student = (Student) o;
+        return score == student.score
+                && Objects.equals(surname, student.surname);
+    }
+
+    /**
+     * Переопределенный метод, для корректного сравнения объектов
+     *
+     * @return возвращает hashcode объекта
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(score, surname);
+    }
+}
