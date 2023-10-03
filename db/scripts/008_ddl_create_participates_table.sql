87554146ddf71727d3ab3201a3ed1bded2d93c37
Artem Chernikov <overcomingjunk@yandex.ru>
2023-10-03 23:23:17 +0300

    feature: many to many relationship

---
+++ db/scripts/008_ddl_create_participates_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE participates
+(
+    id      serial PRIMARY KEY,
+    item_id int not null REFERENCES items (id),
+    user_id int not null REFERENCES j_user (id),
+    UNIQUE (item_id, user_id)
+);
--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,10 +1,13 @@
 package ru.job4j.tracker;
 
 import lombok.*;
+import ru.job4j.toone.User;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -20,6 +23,11 @@ public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
+    @ManyToMany
+    @JoinTable(name = "participates",
+            joinColumns = {@JoinColumn(name = "item_id")},
+            inverseJoinColumns = {@JoinColumn(name = "user_id")})
+    private List<User> participates = new ArrayList<>();
 
     public Item(String name) {
         this.name = name;
